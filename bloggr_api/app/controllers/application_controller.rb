13dfdc551cee1ce863603e94198e053c11ae8978
Jak Charlton <jakcharlton@gmail.com>
2014-12-23 19:58:31 +1100

    Have controller specifically read in the index.html file in public during development
    Relying on the fall through route to then have Rails serve the static HTML file means that #insert_flags does not set any flags - this means development and prod environments are serving different code

--- bloggr_api/app/controllers/application_controller.rb
+++ bloggr_api/app/controllers/application_controller.rb
@@ -3,7 +3,11 @@ class ApplicationController < ActionController::API
   include HtmlHandler
 
   def index
-    @index_html = index_html
+    if Rails.env.development?
+     @index_html =  File.read Rails.root.join('public','index.html')
+    else
+      @index_html = index_html
+    end
     insert_flags 'deploy_key', 'ab_test', 'features'
 
     render text: @index_html
